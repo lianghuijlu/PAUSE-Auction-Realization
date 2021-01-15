846474b85f4df5aaa07c0bb4ce0692282931918e
hui <hui@localhost>
2021-01-15 17:37:21 +0800

    Can run, but most composite values are not bigger then pervious (stage one) composite value.

--- main.m
+++ main.m
@@ -7,18 +7,39 @@ Date: 15 Jan, 2021
 clear
 
 % Parameter Setting
+global ITEMNUM
+ITEMNUM = 4;
 N = 4; % Bidder Number
-I = 4; % Item Number
+I = ITEMNUM; % Item Number
 ItemsPre = [];
 i = 1;
-
+e = 1;
 
 % Varible Setting
-global ALLBID APPCOMBIDLOG
+global ALLBID APPCOMBIDLOG 
 ALLBID = struct;
 APPCOMBIDLOG = struct;
 
 ALLBID = bidGen(N,I);
+bidNum = length(ALLBID);
 disp("ALLBID has done!")
 stageOneComBid(I)
 disp("stageOne has done!")
+
+for t = 2:I
+    for j = 1:bidNum
+        while(ALLBID(j).stage == t)
+            currentBlockBid = ALLBID(j);
+            comBid = comBidGen(currentBlockBid);
+            j = j+1;
+            if (APPCOMBIDLOG.value(i) - comBid.value) < ALLBID(j).value
+               comBid.value = APPCOMBIDLOG.value(i) + e;
+               APPCOMBIDLOG.("block"+1) = ALLBID(j).value + e;
+               APPCOMBIDLOG(i+1) = comBid;
+               i = i+1;
+            end   
+        end
+        j = j+1;
+    end
+end
+    
