6475f7d8ae0633fed08e57d97ed61b51b13e5f2f
hui <hui@localhost>
2021-01-15 20:59:19 +0800

    Solve miss some item-blocks problem, not debug.

--- stageOneComBid.m
+++ stageOneComBid.m
@@ -17,24 +17,35 @@ function stageOneComBid(itemNum)
 
 
 global ALLBID APPCOMBIDLOG
+APPCOMBIDLOG(1).value = 0;
 I = itemNum;
 currentValue = 0;
 j = 1;
-APPCOMBIDLOG(1).value = 0;
+k = 1;
+allItem = [1:I];
+mentionedItem = [];
 
 for i = 1:I
     while(ALLBID(j).stage == 1)
         if ALLBID(j).item == i
-            if ALLBID(j).value > currentValue
+            if ALLBID(j).value >= currentValue
                 currentValue = ALLBID(j).value;
                 APPCOMBIDLOG(1).("block"+i) = ALLBID(j);
             end
         end
         j = j+1;
+        mentionedItem = [mentionedItem i];
     end
     j = 1;
     APPCOMBIDLOG(1).value = APPCOMBIDLOG(1).value + currentValue;
     currentValue = 0;
+end
+    
+if (numel(fieldnames(APPCOMBIDLOG))-1) ~= itemNum
+    suppItem = setxor(mentionedItem,allItem);
+    for m = 1:numel(suppItem)
+        APPCOMBIDLOG(1).("block"+suppItem(m))=[];
+    end       
 end
 
 end
