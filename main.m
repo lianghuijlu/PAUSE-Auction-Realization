d86eaa6ee5473c793b072678e5412f108d92aa99
hui <hui@localhost>
2021-01-15 15:21:52 +0800

    bidGen() and stageOneCombid() have done.

--- main.m
+++ main.m
@@ -1,6 +1,24 @@
+%{
+This is the main function.
+Author: Hui Liang
+Date: 15 Jan, 2021
+%}
+
 clear
 
+% Parameter Setting
 N = 4; % Bidder Number
 I = 4; % Item Number
 ItemsPre = [];
 i = 1;
+
+
+% Varible Setting
+global ALLBID APPCOMBIDLOG
+ALLBID = struct;
+APPCOMBIDLOG = struct;
+
+ALLBID = bidGen(N,I);
+disp("ALLBID has done!")
+stageOneComBid(I)
+disp("stageOne has done!")
